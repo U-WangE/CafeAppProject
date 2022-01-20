65f19ea1e75b5567ad5d7d3f711b68030449074b
U-WangE <chamchamm@naver.com>
2022-01-20 16:08:01 +0900

    기초 디자인

--- app/src/main/java/com/example/cafeappproject/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/LoginFragment.kt
@@ -6,14 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cafeappproject.R
+import com.example.cafeappproject.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: FragmentLoginBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        mBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.btnSignup
+        return binding.root
+    }
+
+    // 프래그먼트가 destroy (파괴) 될때..
+    override fun onDestroyView() {
+        // onDestroyView 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroyView()
     }
 }
