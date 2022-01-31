20b3971f24ded67c484229b7356697a258711dde
U-WangE <chamchamm@naver.com>
2022-01-31 20:51:34 +0900

    [add]cloud firestore 중복 검사 함수 완성

--- app/src/main/java/com/example/cafeappproject/fragment/OverlapOrRulesCheck.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/OverlapOrRulesCheck.kt
@@ -1,13 +1,15 @@
 package com.example.cafeappproject.fragment
 
+import android.service.autofill.UserData
 import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class OverlapOrRulesCheck {
-
-    private val mDatabase = FirebaseDatabase.getInstance().getReference();
-
+    val mDatabase = Firebase.firestore
+    
     public fun NicknameRulesCheck(nickname: String?) : String {
         val exp = Regex("^[가-힣ㄱ-ㅎa-zA-Z0-9._ -]{2,}\$")  // password 정규식(숫자, 영어, 한국어와 언더스코어) 허용
 
@@ -16,20 +18,32 @@ class OverlapOrRulesCheck {
         else if(!IsOverlap(nickname, "nickname")) return "CurrectInput" else return "Overlap" // database 에서 닉네임 가져와 중복 검사
     }
 
-    // email 저장
     public fun EmailRulesCheck(email: String): String {
         val exp = Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\$")  // email 정규식
+
+        IsOverlap(email, "email");
 
         if(email.isNullOrEmpty()) return "NullOrEmpty"
         else if(!exp.matches(email)) return "WrongInput"
         else if(!IsOverlap(email, "email")) return "CurrectInput" else return "Overlap" // database 에서 이메일 가져와 중복 검사
     }
 
+    // cloud firestore  내의 중복 값 확인 (x realtime database)
     private fun IsOverlap(text: String, type: String): Boolean {
-        when(type) {
-            //"nickname" ->
-        }
+        var isOverlap = false
 
-        return false
+        mDatabase.collection("member")
+            .get()
+            .addOnSuccessListener { result ->
+                for(document in result) {
+                    if(document.data.getValue(type).equals(text))  // 중복 값 판별
+                        isOverlap = true
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Overlap => ", "Error getting Overlap", exception)
+            }
+
+        return isOverlap
     }
 }
