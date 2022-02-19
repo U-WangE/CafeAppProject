0807a28ae7eb0932b47cd4b821ece5bc21f25e64
yoonmin <dmg04158@gmail.com>
2022-02-19 23:04:16 +0900

    Frag_Main Layout Change
    Basic hierarchical layout works

--- app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import com.example.cafeappproject.R
+import com.example.cafeappproject.databinding.FragmentSignUpBinding
 
 class MainFragment : Fragment() {
+
+    private var mBinding: FragmentSignUpBinding? = null
+    private val binding get() = mBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
