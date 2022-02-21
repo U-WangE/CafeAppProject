f9a8c85dc4ce391269ee8f197435acd3146008ba
yoonmin <dmg04158@gmail.com>
2022-02-22 01:00:45 +0900

    Main Frag Adjustable
    *이미지 슬라이더 작동안함

--- app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
@@ -41,38 +41,3 @@ class MainFragment : Fragment() {
 
 
 }
-
-
-class ViewPagerAdapter(private val context: Context) : PagerAdapter() {
-    private var layoutInflater: LayoutInflater? = null
-
-    var images = arrayOf(
-        R.drawable.slider_coffee
-    )
-
-    override fun getCount(): Int {
-        return images.size
-    }
-
-    override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        return view === `object`
-    }
-
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val v = layoutInflater!!.inflate(R.layout.fragment_main_sliderimage, null)
-        val image = v.findViewById<View>(R.id.slider_imageView) as ImageView
-
-        image.setImageResource(images[position])
-        val vp = container as ViewPager
-        vp.addView(v, 0)
-
-        return v
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        val vp = container as ViewPager
-        val v = `object` as View
-        vp.removeView(v)
-    }
-}
