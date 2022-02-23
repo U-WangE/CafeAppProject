8e311efa801b7dee57c8f6c253a4808c62112764
yoonmin <dmg04158@gmail.com>
2022-02-23 15:50:45 +0900

    Fragment Drawer Test
    테스트성공

--- app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/MainFragment.kt
@@ -3,13 +3,16 @@ package com.example.cafeappproject.fragment
 import android.content.Context
 import android.media.Image
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.cafeappproject.R
@@ -20,10 +23,8 @@ import com.smarteist.autoimageslider.SliderViewAdapter
 
 class MainFragment : Fragment() {
 
-    private var mBinding: FragmentSignUpBinding? = null
+    private var mBinding: FragmentMainBinding? = null
     private val binding get() = mBinding!!
-
-    internal lateinit var viewpager : ViewPager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,5 +40,18 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val drawer = getView()?.findViewById<DrawerLayout>(R.id.id_drawer)
+        val button_toolbar = getView()?.findViewById<ImageButton>(R.id.id_btn_toolbar_menu)
+        button_toolbar?.setOnClickListener {
+            if (drawer!!.isDrawerOpen(Gravity.LEFT)) {
+                drawer.closeDrawer(Gravity.LEFT)
+            }
+            else {
+                drawer.openDrawer(Gravity.LEFT)
+            }
+        }
+    }
 }
