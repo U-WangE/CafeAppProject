dd5502fb81a5f4f7bca76532da939e48068cd18f
U-WangE <chamchamm@naver.com>
2022-03-14 19:27:03 +0900

    [add]profile spinner 기능 추가 중

--- app/src/main/java/com/example/cafeappproject/fragment/ProfileActivity.kt
+++ app/src/main/java/com/example/cafeappproject/fragment/ProfileActivity.kt
@@ -2,11 +2,55 @@ package com.example.cafeappproject.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.navigation.findNavController
 import com.example.cafeappproject.R
+import com.example.cafeappproject.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private var mBinding: ActivityProfileBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        mBinding = ActivityProfileBinding.inflate(layoutInflater)
+
+        var spinner = binding.idSpinnerProfileSpinner
+        spinner.adapter = ArrayAdapter.createFromResource(this, R.array.my_Profile_spinner, android.R.layout.simple_spinner_item)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position) {
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    2 -> {
+
+                    }
+                    else -> {
+                        Log.e("ProfileSpinner => ", "Error getting ProfileSpinner")
+                    }
+                }
+            }
+
+
+        }
+
+
+        setContentView(binding.root)
     }
 }
